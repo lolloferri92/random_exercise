4b533620ad11f724065d41b0eaeb5c1e74da44a1
lollo2 <lolloferri92@gmail.com>
2022-10-14 21:55:10 +0200

    la lilla mi vuole male ma io pragrammo lo stesso

---
+++ cicli while e for.py
@@ -0,0 +1,37 @@
+#programma per fare i cicli while e for comparati
+print("lista con ciclo while")
+i=0
+while i<10:
+    print (i)
+    i+=1
+print("lista con ciclo for")
+for counter in range(10):
+    print(counter)
+
+#programma per fare i cicli while e for comparati con intervallo di 3 e partendo da 2 e finendo a 15
+print ("programma per fare i cicli while e for comparati con intervallo di 3 e partendo da 2 e finendo a 15")
+i=3
+while i<=15:
+    print(i)
+    i+=2
+
+for u in range(3,16,2):
+    print(u)
+
+def paricheck(x):
+    if x%2==0:
+        return True
+    else:
+        False
+print ("lista con ciclo while break e continue")
+#esempio di ciclo infinito
+infinite=0
+i=0
+while infinite==0:
+    if i==26:
+        break
+   # elif paricheck(i)!=True:
+       # continue
+    else:
+        print("infinite" + str(i))
+        i+=1
--- cicli while.py
+++
@@ -1,7 +0,0 @@
-#programma per fare i cicli while
-
-i=0
-while i<10:
-    print (i)
-    i+=1
-

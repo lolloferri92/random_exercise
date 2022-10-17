4dd4a2c3b9b763d56e3690fba806a50058069a00
lollo2 <lolloferri92@gmail.com>
2022-10-17 12:47:43 +0200

    classe

--- classe.py
+++ classe.py
@@ -1,15 +1,15 @@
 class studente:
-    def _init_(self,nome,cognome,corso_di_studi):
+    def __init__(self,nome,cognome,corso_di_studi):
         self.nome=nome
         self.cognome=cognome
         self.corso_di_studi=corso_di_studi
 
     def scheda_personale(self):
-        return
-        #return f"Scheda Studente\n Nome:{self.nome}\n Cognome:{self.cognome}\n Corso Di Studi:{self.corso_di_studi}"
+        return "Scheda Studente\n Nome: self.nome}\n Cognome:{self.cognome}\n Corso Di Studi:{self.corso_di_studi}"
 
 studente_uno=studente("pippo","sempronio","algebra")
-print(studente_uno.scheda_personale())
 
+print(studente_uno.nome)
+print(studente_uno.scheda_personale())
 
 

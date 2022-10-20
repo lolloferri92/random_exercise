343d8990bde8f58bdbbc81dff8b8656548c04a44
lollo2 <lolloferri92@gmail.com>
2022-10-20 20:26:40 +0200

    project insertion on github repository

--- classe.py
+++ classe.py
@@ -5,11 +5,9 @@ class studente:
         self.corso_di_studi=corso_di_studi
 
     def scheda_personale(self):
-        return "Scheda Studente\n Nome: self.nome}\n Cognome:{self.cognome}\n Corso Di Studi:{self.corso_di_studi}"
+        return f"Scheda Studente\n Nome: {self.nome}\n Cognome:{self.cognome}\n Corso Di Studi:{self.corso_di_studi}"
 
 studente_uno=studente("pippo","sempronio","algebra")
-
 print(studente_uno.nome)
+print(studente_uno.cognome)
 print(studente_uno.scheda_personale())
-
-
